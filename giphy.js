000b5f4b9fa940965cfb05d4361f6e32d133d49f
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2022-07-02 00:59:01 -0500

    added gif logic

--- giphy.js
+++ giphy.js
@@ -1,20 +1,23 @@
-async function getGif(gif) {
-    let funGif = await axios.get(`https://dog.ceo/api/breed/${doggy}/images/random`);
+function getGif(gif) {
     let img = document.createElement("img");
-    img.src = dogPic.data.message;
+    let random = Math.floor(Math.random() * gif.data.length);
+    img.src = gif.data[random].images.original.url;
     img.className = "col-4"
     let imgBox = document.querySelector("#img-box");
     imgBox.append(img);
 }
+
 let searchButton = document.querySelector("#searchButton");
-searchButton.addEventListener("click", function (event) {
+let removeButton = document.querySelector("#removeButton");
+searchButton.addEventListener("click", async function (event) {
     event.preventDefault();
     let input = document.getElementById("search");
     let inputVal = input.value;
-    if (inputVal.includes(" ")) {
-        let editedArr = inputVal.toLowerCase().split(" ");
-        let editedVal = editedArr[editedArr.length - 1] + "/" + editedArr[0];
-        getDogPic(editedVal);
-        input.value = '';
-    }
+    let editedVal = inputVal.toLowerCase();
+    let funGif = await axios.get("https://api.giphy.com/v1/gifs/search", { params: { api_key: "meyXoJBsuJ36UNsNcCgu9X1fYIIp9Cz8", q: editedVal } });
+    getGif(funGif.data);
+    input.value = '';
+});
+removeButton.addEventListener("click", function () {
+    localStorage.reload();
 });
